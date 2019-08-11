7209ca167f98ba9ba2d1942cda6e2fa4e13b6571
dsjodha <43707696+dsjodha@users.noreply.github.com>
2019-08-11 19:17:15 +0530

    Add files via upload

---
+++ src/main/java/com/journaldev/daynightmode/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.journaldev.daynightmode;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = findViewById(R.id.button);
+        Configuration configuration = null;
+        int currentNightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                break;
+        }
+    }
+}
